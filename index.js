4938d460831b2763284aff5785862414b9a88ba8
Hugo <hugo.benjaminsson1@gmail.com>
2025-04-29 13:06:31 +0200

    navbar development

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
+let instructionsBTN = document.querySelector(".instructionsBtn");
+let instructionsDropDown = document.querySelector(".instructionsDropDown");
 
-  async function fetchLastUpdated() {
-    try {
-      const response = await fetch("https://api.github.com/repos/Arvid459/WEU2-instruduktionshemsida/commits");
-      const data = await response.json();
-      const latestCommit = data[0].commit.committer.date;
-      const formattedDate = new Date(latestCommit).toLocaleDateString("sv-SE", {
-        year: "numeric", month: "long", day: "numeric"
-      });
-      document.getElementById("lastUpdate").textContent = `Senast uppdaterad: ${formattedDate}`;
-    } catch (error) {
-      document.getElementById("lastUpdate").textContent = "Kunde inte hämta uppdateringsdatum.";
-      console.error(error);
-    }
+instructionsBTN.onclick = function () {
+  changeVisibility(instructionsDropDown);
+};
+
+document.addEventListener("click", function (e) {
+  if (!e.target.closest(".instructionsDropDown-container")) {
+    instructionsDropDown.style.display = "none";
   }
+});
 
-  fetchLastUpdated();
+function changeVisibility(e) {
+  e.style.display = e.style.display === "block" ? "none" : "block";
+}
 
+async function fetchLastUpdated() {
+  try {
+    const response = await fetch(
+      "https://api.github.com/repos/Arvid459/WEU2-instruduktionshemsida/commits"
+    );
+    const data = await response.json();
+    const latestCommit = data[0].commit.committer.date;
+    const formattedDate = new Date(latestCommit).toLocaleDateString("sv-SE", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+    document.getElementById(
+      "lastUpdate"
+    ).textContent = `Senast uppdaterad: ${formattedDate}`;
+  } catch (error) {
+    document.getElementById("lastUpdate").textContent =
+      "Kunde inte hämta uppdateringsdatum.";
+    console.error(error);
+  }
+}
+
+fetchLastUpdated();
